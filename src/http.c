49b739ddfab1edec60787a0138694bc24c327c3c
Luís Correia <68921330+LuisPCNeri@users.noreply.github.com>
2025-11-11 20:50:14 +0000

    Added send_file function as written in the network programming tutorial file.

--- src/http.c
+++ src/http.c
@@ -14,6 +14,50 @@ const char* response =
     "\r\n"
     "<html><body><h1>Hello, World! HEHEHEHA</h1></body></html>";
 
+// Sends file in cosnt char* path to int client_fd
+void send_file(int client_fd, const char* path){
+    FILE* file = fopen(path, "rb");
+
+    if(!file){
+        // Send 404 Error to user
+        const char* response =
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/html\r\n"
+            "\r\n"
+            "<h1>404 Not Found</h1>";
+        send(client_fd, response, strlen(response), 0);
+        return;
+    }
+
+    // Get file size
+    // Goes to the END of the file
+    fseek(file, 0, SEEK_END);
+    // Returns the position of given stream in this case the END of file
+    long file_size = ftell(file);
+    // Puts the stream back at START of file
+    fseek(file, 0, SEEK_SET);
+
+    // Sends ONLY the file header
+
+    char file_header[512];
+    snprintf(file_header, sizeof(file_header), 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: %ld\r\n"
+        "\r\n", file_size);
+    send(client_fd, file_header, strlen(file_header), 0);
+
+    char buffer[BUFFER_SIZE];
+    size_t bytes;
+
+    while( (bytes = fread(buffer, 1, sizeof(buffer), file)) > 0 ){
+        // While any non zero number of bytes are being read
+        send(client_fd, buffer, bytes, 0);
+    }
+
+    fclose(file);
+} 
+
 int main(void){
 
     // Created socket using IPv4 sockets (AF_INET) as TCP packets (SOCK_STREAM) without specifying a protocol
@@ -68,6 +112,8 @@ int main(void){
 
         // SENDS const void * buf MESSAGE WITH size_t n LENGTH to int __fd SOCKET returns number of bytes sent
         send(client_fd, response, strlen(response), 0);
+
+        send_file(client_fd, "/home/luisp/concurrent-http-server/test.txt");
         // Closes connection
         close(client_fd);
     }
