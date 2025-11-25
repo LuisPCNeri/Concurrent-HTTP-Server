87b1ed4161c7580aa5027089e8d6a2c6840c2310
Luis Correia <luispedrocneri@gmail.com>
2025-11-25 12:26:39 +0000

    more files

---
+++ www/script.js
@@ -0,0 +1,22 @@
+const BUTTON = document.getElementById("btn");
+
+var obj;
+
+function get_request(body_element){
+    fetch("http://localhost:8080")
+    .then(response => response)
+    .then(data => {
+        console.log(data);
+        // Makes server's response body appear in html page
+        data.text().then( text => {
+            console.log(text);
+            //document.getElementById("text").innerHTML = text;
+            body_element.innerHTML = text;
+        });
+    })
+    .catch(error => console.log(error));
+}
+
+BUTTON.onclick = () => {
+    get_request(document.getElementById("text"));
+};
